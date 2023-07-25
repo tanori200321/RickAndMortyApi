71f9764edde254e262303abf6d5770426cfd0087
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-07-25 12:13:10 -0700

    Add project files.

---
+++ RickAndMortyApi/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Newtonsoft.Json;
+using RickAndMortyApi;
+using System.Diagnostics;
+using System.Net.Http.Json;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://rickandmortyapi.com/") });
+
+await builder.Build().RunAsync();
