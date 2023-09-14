39fbcbf4b925eb917142befd9d00922b2e8dbef7
Sebastian Gpe Tanori Ruiz <tanoriruizs@gmail.com>
2023-09-14 11:59:04 -0700

    Separacion del codigo y design

---
+++ RickAndMortyApi/Pages/Characters.razor.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components;
+using RickAndMortyApi.Models;
+
+namespace RickAndMortyApi.Pages
+{
+    public partial class Characters
+    {
+        private List<Character> characters = new List<Character>();
+        private bool showTable = false;
+        private bool showCharacterDetails = false;
+        private int selectedCharacterId;
+        private string selectedGenderFilter = "All";
+
+        [Inject] NavigationManager NavigationManager { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCharactersAsync();
+        }
+
+        private async Task LoadCharactersAsync()
+        {
+            var response = await Http.GetAsync("https://rickandmortyapi.com/api/character");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<ApiResponse>(json);
+                characters = result?.Results;
+            }
+            else
+            {
+                characters = new List<Character>();
+            }
+        }
+
+        private void ShowInformation()
+        {
+            showTable = true;
+        }
+
+        private void HideInformation()
+        {
+            showTable = false;
+            HideCharacterDetails();
+        }
+
+        private void ShowCharacterDetails(int characterId)
+        {
+            selectedCharacterId = characterId;
+            showCharacterDetails = true;
+        }
+
+        private void HideCharacterDetails()
+        {
+            showCharacterDetails = false;
+        }
+
+        private void SetGenderFilter(ChangeEventArgs e)
+        {
+            selectedGenderFilter = e.Value.ToString();
+        }
+
+        public class ApiResponse
+        {
+            public List<Character>? Results { get; set; }
+        }
+    }
+}
---
+++ RickAndMortyApi/Pages/CharactersDetails.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using RickAndMortyApi.Models;
+
+
+namespace RickAndMortyApi.Pages
+{
+    public partial class CharactersDetails
+    {
+        private Character character;
+
+        [Parameter]
+        public int CharacterId { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCharacterAsync();
+        }
+
+        private async Task LoadCharacterAsync()
+        {
+            var response = await Http.GetAsync($"https://rickandmortyapi.com/api/character/{CharacterId}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                character = Newtonsoft.Json.JsonConvert.DeserializeObject<Character>(json);
+            }
+            else
+            {
+                character = null;
+            }
+        }
+
+    }
+}
